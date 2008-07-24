975ce3a1a5bb55df11adaefb8197e41d4ca8e32a
Rodrigo Carneiro Henrique <rodrigoh@tecgraf.puc-rio.br>
2008-07-24 20:51:37 +0000

    Adição de classe auxiliar para a criação de membros de sessão.
    git-svn-id: https://subversion.tecgraf.puc-rio.br/engdist/openbus/trunk/core/utilities/java@80430 ae0415b3-e90b-0410-900d-d0be9363c56b

---
+++ openbus/common/session/SessionMember.java
@@ -0,0 +1,92 @@
+/*
+ * $Id$
+ */
+package openbus.common.session;
+
+import java.util.ArrayList;
+
+import openbusidl.ss.SessionEventSinkPOA;
+
+import org.omg.CORBA.UserException;
+import org.omg.PortableServer.POA;
+
+import scs.core.ComponentId;
+import scs.core.FacetDescription;
+import scs.core.servant.IComponentServant;
+
+/**
+ * Representa um componente membro de uma sessão.
+ * 
+ * @author Tecgraf/PUC-Rio
+ */
+public final class SessionMember extends IComponentServant {
+  /**
+   * O nome da faceta de um receptor de eventos de uma sessão.
+   */
+  private static final String EVENT_SINK_NAME = "eventSink";
+  /**
+   * O nome da interface de um receptor de eventos de uma sessão.
+   */
+  private static final String EVENT_SINK_INTERFACE =
+    "IDL:openbusidl/ss/SessionEventSink:1.0";
+  /**
+   * O POA.
+   */
+  private POA poa;
+  /**
+   * O receptor de eventos da sessão.
+   */
+  private org.omg.CORBA.Object eventSink;
+
+  /**
+   * Cria um componente membro de uma sessão.
+   * 
+   * @param poa O POA.
+   * @param eventSink O receptor de eventos da sessão.
+   * 
+   * @throws UserException Caso ocorra algum erro na exportação do receptor de
+   *         eventos.
+   */
+  public SessionMember(POA poa, SessionEventSinkPOA eventSink)
+    throws UserException {
+    this.poa = poa;
+    this.eventSink = this.poa.servant_to_reference(eventSink);
+  }
+
+  /**
+   * {@inheritDoc}
+   */
+  @Override
+  protected ComponentId createComponentId() {
+    return new ComponentId(this.getClass().getName(), 1);
+  }
+
+  /**
+   * {@inheritDoc}
+   */
+  @Override
+  protected ArrayList<FacetDescription> createFacets() {
+    FacetDescription description =
+      new FacetDescription(EVENT_SINK_NAME, EVENT_SINK_INTERFACE,
+        this.eventSink);
+    ArrayList<FacetDescription> facets = new ArrayList<FacetDescription>();
+    facets.add(description);
+    return facets;
+  }
+
+  /**
+   * {@inheritDoc}
+   */
+  @Override
+  protected boolean doStartup() {
+    return true;
+  }
+
+  /**
+   * {@inheritDoc}
+   */
+  @Override
+  protected boolean doShutdown() {
+    return true;
+  }
+}
