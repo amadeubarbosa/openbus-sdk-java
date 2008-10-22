5c6345bfed0a7667f56716a3aba7b53ef6279d7a
Rodrigo Carneiro Henrique <rodrigoh@tecgraf.puc-rio.br>
2008-10-22 19:44:36 +0000

    Pode ser criada tanto com exceções de runtime (SystemException) quanto com exceções checadas (UserException).
    git-svn-id: https://subversion.tecgraf.puc-rio.br/engdist/openbus/trunk/core/utilities/java@84611 ae0415b3-e90b-0410-900d-d0be9363c56b

--- openbus/exception/CORBAException.java
+++ openbus/exception/CORBAException.java
@@ -6,6 +6,7 @@ package openbus.exception;
 import openbus.common.exception.OpenBusException;
 
 import org.omg.CORBA.SystemException;
+import org.omg.CORBA.UserException;
 
 /**
  * Representa uma exceção gerada pelo <i>runtime</i> de CORBA.
@@ -19,6 +20,15 @@ public final class CORBAException extends OpenBusException {
    * @param cause A causa.
    */
   public CORBAException(SystemException cause) {
+    super(cause);
+  }
+
+  /**
+   * Cria a exceção com uma causa associada.
+   * 
+   * @param cause A causa.
+   */
+  public CORBAException(UserException cause) {
     super(cause);
   }
 }
