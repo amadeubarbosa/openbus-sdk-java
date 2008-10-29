62392dfd7e4bf2d2a9a37d9b873efb540bc6148c
Rodrigo Carneiro Henrique <rodrigoh@tecgraf.puc-rio.br>
2008-10-29 17:41:24 +0000

    [OPENBUS-88]: Mover a estrutura DataChannel de project_service.idl para data_service.idl
    git-svn-id: https://subversion.tecgraf.puc-rio.br/engdist/openbus/trunk/core/utilities/java@84907 ae0415b3-e90b-0410-900d-d0be9363c56b

--- openbus/InvalidTypes.java
+++ openbus/InvalidTypes.java
@@ -4,6 +4,7 @@
 package openbus;
 
 import openbusidl.acs.Credential;
+import openbusidl.ds.DataChannel;
 import scs.core.ComponentId;
 
 /**
@@ -17,9 +18,13 @@ public final class InvalidTypes {
    * Representa um identificador de componente inválido.
    */
   public static final ComponentId COMPONENT_ID = new ComponentId("", -1);
-
   /**
    * Representa uma credencial inválida.
    */
   public static final Credential CREDENTIAL = new Credential("", "", "");
+  /**
+   * Representa um canal de dados inválido.
+   */
+  public static final DataChannel DATA_CHANNEL =
+    new DataChannel("", -1, new byte[0], new byte[0], false, -1);
 }
