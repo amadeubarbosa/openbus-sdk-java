bf4722c2ed106cced59b1f073f6bc03792a71e59
Rodrigo Carneiro Henrique <rodrigoh@tecgraf.puc-rio.br>
2009-04-17 21:50:25 +0000

    Adição de testes para validar um certificado digital e uma chave privada.
    git-svn-id: https://subversion.tecgraf.puc-rio.br/engdist/openbus/trunk/core/utilities/java@90694 ae0415b3-e90b-0410-900d-d0be9363c56b

---
+++ test/src/openbus/util/CertificateValidator.java
@@ -0,0 +1,25 @@
+package openbus.util;
+
+import java.io.IOException;
+import java.security.GeneralSecurityException;
+import java.security.cert.X509Certificate;
+
+import openbus.common.CryptoUtils;
+
+public final class CertificateValidator {
+	public static void main(String[] args) throws GeneralSecurityException,
+			IOException {
+		if (args.length == 0) {
+			System.out.print("O arquivo de certificado não foi especificado.");
+			System.out.println(" Use: CertificateValidator <file>");
+			System.exit(0);
+		}
+		String certificateFile = args[0];
+		System.out.println("Arquivo a ser verificado: " + certificateFile);
+		X509Certificate certificate = CryptoUtils
+				.readCertificate(certificateFile);
+		System.out.println("Certificado carregado com sucesso.");
+		System.out.print("Tipo: ");
+		System.out.println(certificate.getType());
+	}
+}
