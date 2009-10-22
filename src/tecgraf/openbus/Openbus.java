7a2308aa2bbd58d9fa0d80e9df2837baa1e3744b
Hugo Roenick <hroenick@tecgraf.puc-rio.br>
2009-10-22 21:41:01 +0000

    Commit parcial do item [OPENBUS-206]: Atualizar serviços básicos para que utilizem receptáculos
    - correções e ajustes (email de comentários do Cadu - http://listas.tecgraf.puc-rio.br/pipermail/openbus-dev/2009-October/001442.html)
    
    OBS: Testes unitários de Lua não estão passando por todos os testes.
    
    git-svn-id: https://subversion.tecgraf.puc-rio.br/engdist/openbus/trunk/core/utilities/java@97972 ae0415b3-e90b-0410-900d-d0be9363c56b

--- src/tecgraf/openbus/Openbus.java
+++ src/tecgraf/openbus/Openbus.java
@@ -94,7 +94,7 @@ public final class Openbus {
    */
   private ILeaseProvider lp;
   /**
-   * Interface ILeaseProvider do Serviço de Controle de Acesso.
+   * Interface IComponent do Serviço de Controle de Acesso.
    */
   private IComponent ic;
   /**
