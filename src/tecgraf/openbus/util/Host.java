e9b18895f1d0f2f24db6bd198221aa6dae633aab
Mauricio Arieira Rosas <marosas@tecgraf.puc-rio.br>
2010-08-02 20:37:07 +0000

    Formatacao
    git-svn-id: https://subversion.tecgraf.puc-rio.br/engdist/openbus/sdk/java/trunk@108595 ae0415b3-e90b-0410-900d-d0be9363c56b

--- src/tecgraf/openbus/util/Host.java
+++ src/tecgraf/openbus/util/Host.java
@@ -1,29 +1,25 @@
 package tecgraf.openbus.util;
 
-public class Host
-{
+public class Host {
 
-   public String hostName;
-   
-   
-   public String getHostName() {
-	   return hostName;
-   }
-   
-   public int getHostPort() {
-	return hostPort;
-   }
-   
-   
-   public int hostPort;
-   
-   /**
-    * @param hostName
-    * @param hostPort
-    */
-   public Host(String hostName, int hostPort)
-   {
-      this.hostName = hostName;
-      this.hostPort = hostPort;
-   }
+  public String hostName;
+
+  public String getHostName() {
+    return hostName;
+  }
+
+  public int getHostPort() {
+    return hostPort;
+  }
+
+  public int hostPort;
+
+  /**
+   * @param hostName
+   * @param hostPort
+   */
+  public Host(String hostName, int hostPort) {
+    this.hostName = hostName;
+    this.hostPort = hostPort;
+  }
 }
