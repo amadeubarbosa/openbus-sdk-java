d3ba09f7e287bbc90a492cd7ae53c379ad90142c
Mauricio Arieira Rosas <marosas@tecgraf.puc-rio.br>
2010-11-01 18:48:23 +0000

    [OPENBUS-1057] Documentar o comportamento do metodo que se conecta por credencial
    git-svn-id: https://subversion.tecgraf.puc-rio.br/engdist/openbus/sdk/java/trunk@112019 ae0415b3-e90b-0410-900d-d0be9363c56b

--- src/tecgraf/openbus/Openbus.java
+++ src/tecgraf/openbus/Openbus.java
@@ -635,8 +635,16 @@ public final class Openbus {
   }
 
   /**
+   * <p>
    * Realiza uma tentativa de conexão com o barramento(serviço de controle de
-   * acesso e o serviço de registro), a partir de uma credencial.
+   * acesso), a partir de uma credencial.
+   * </p>
+   * 
+   * <p>
+   * Este processo utiliza a conexão do dono da credencial. Logo ele não é
+   * responsável por manter a conexão ativa - não renova o <i>lease</i>. Se o
+   * processo dono da credencial se desconectar, este será desconectado.
+   * </p>
    * 
    * @param credential A credencial.
    * 
