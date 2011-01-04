467246315685719ca358b428c7b7a487e2df4465
Rodrigo Carneiro Henrique <rodrigoh@tecgraf.puc-rio.br>
2011-01-04 20:19:43 +0000

    [OPENBUS-1158]: Criar script para preparar o ambiente do teste unitário
    git-svn-id: https://subversion.tecgraf.puc-rio.br/engdist/openbus/sdk/java/trunk@114255 ae0415b3-e90b-0410-900d-d0be9363c56b

---
+++ prepare_test_environment.sh
@@ -0,0 +1,36 @@
+#!/bin/bash
+
+if [ $# -ne 1 ]
+then
+  echo "Use: `basename $0` <admin_user>"
+  exit
+fi
+
+if [ ! -f ${OPENBUS_HOME}/data/certificates/AccessControlService.crt ]
+then
+  echo "O certificado digital do barramento \
+${OPENBUS_HOME}/data/certificates/AccessControlService.crt \
+não foi encontrado."
+  exit
+fi
+
+if [ ! -f ${OPENBUS_HOME}/bin/openssl-generate.ksh ]
+then
+  echo "O script para geração de certificados \
+${OPENBUS_HOME}/bin/openssl-generate.ksh \
+não foi encontrado."
+  exit
+fi
+
+if [ ! -f ${OPENBUS_HOME}/core/bin/run_management.sh ]
+then
+  echo "O script de governança do barramento \
+${OPENBUS_HOME}/core/bin/run_management.sh \
+não foi encontrado."
+  exit
+fi
+
+(cd test/resources;
+cp ${OPENBUS_HOME}/data/certificates/AccessControlService.crt openbus.crt;
+${OPENBUS_HOME}/bin/openssl-generate.ksh -n sdk_java_core;
+${OPENBUS_HOME}/core/bin/run_management.sh --login=$1 --script=sdk_java.mgt)
