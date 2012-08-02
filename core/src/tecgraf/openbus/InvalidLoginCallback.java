f3c6478c391dba680f0397896d73cfc3e8236969
Hugo Roenick <hroenick@tecgraf.puc-rio.br>
2012-08-02 12:26:56 +0000

    Commit parcial do item [OPENBUS-1898]/[OPENBUS-1939}: (Java) Documentar API da biblioteca de acesso do OpenBus 2.0  - incluindo informação de caso ocorra uma exceção na execução da calback o erro será apenas logado.
    git-svn-id: https://subversion.tecgraf.puc-rio.br/engdist/openbus/sdk/java/branches/v2_proto@131797 ae0415b3-e90b-0410-900d-d0be9363c56b

--- core/src/tecgraf/openbus/InvalidLoginCallback.java
+++ core/src/tecgraf/openbus/InvalidLoginCallback.java
@@ -16,7 +16,9 @@ public interface InvalidLoginCallback {
    * Callback de login inválido.
    * <p>
    * Método a ser implementada pelo objeto de callback a ser chamado quando uma
-   * notificação de login inválido é recebida.
+   * notificação de login inválido é recebida. Caso alguma exceção ocorra
+   * durante a execução do método e não seja tratada, o erro será capturado pelo
+   * interceptador e registrado no log.
    * 
    * @param conn Conexão que recebeu a notificação de login inválido.
    * @param login Informações do login que se tornou inválido.
