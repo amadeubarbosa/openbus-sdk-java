f552a7ee913b158925a5985927a3e724af7b0ea9
Hugo Roenick <hroenick@tecgraf.puc-rio.br>
2012-09-04 17:25:19 +0000

    Commit parcial do item [OPENBUS-1732]/[OPENBUS-1733]: [JAVA] Implementar demos para o SDK 2.0  - implementando o demo greeting
    git-svn-id: https://subversion.tecgraf.puc-rio.br/engdist/openbus/sdk/java/trunk@133049 ae0415b3-e90b-0410-900d-d0be9363c56b

---
+++ demo/greetings/src/demo/GreetingsServer.java
@@ -0,0 +1,209 @@
+package demo;
+
+import java.util.HashMap;
+import java.util.Map.Entry;
+
+import org.omg.CORBA.COMM_FAILURE;
+import org.omg.CORBA.NO_PERMISSION;
+import org.omg.CORBA.ORB;
+import org.omg.CORBA.TRANSIENT;
+import org.omg.CORBA.ORBPackage.InvalidName;
+import org.omg.PortableServer.POA;
+import org.omg.PortableServer.POAHelper;
+import org.omg.PortableServer.POAManagerPackage.AdapterInactive;
+
+import scs.core.ComponentContext;
+import scs.core.ComponentId;
+import scs.core.exception.SCSException;
+import tecgraf.openbus.Connection;
+import tecgraf.openbus.OpenBusContext;
+import tecgraf.openbus.core.ORBInitializer;
+import tecgraf.openbus.core.OpenBusPrivateKey;
+import tecgraf.openbus.core.v2_0.services.ServiceFailure;
+import tecgraf.openbus.core.v2_0.services.access_control.AccessDenied;
+import tecgraf.openbus.core.v2_0.services.access_control.MissingCertificate;
+import tecgraf.openbus.core.v2_0.services.access_control.NoLoginCode;
+import tecgraf.openbus.core.v2_0.services.offer_registry.InvalidProperties;
+import tecgraf.openbus.core.v2_0.services.offer_registry.InvalidService;
+import tecgraf.openbus.core.v2_0.services.offer_registry.ServiceProperty;
+import tecgraf.openbus.core.v2_0.services.offer_registry.UnauthorizedFacets;
+import tecgraf.openbus.demo.util.Utils;
+import tecgraf.openbus.exception.AlreadyLoggedIn;
+import demo.GreetingsImpl.Language;
+import demo.GreetingsImpl.Period;
+
+/**
+ * Parte servidora do demo Hello
+ * 
+ * @author Tecgraf
+ */
+public final class GreetingsServer {
+
+  /**
+   * Função principal.
+   * 
+   * @param args argumentos.
+   * @throws InvalidName
+   * @throws AdapterInactive
+   * @throws SCSException
+   * @throws AlreadyLoggedIn
+   * @throws ServiceFailure
+   * @throws InvalidService
+   * @throws InvalidProperties
+   */
+  public static void main(String[] args) throws InvalidName, AdapterInactive,
+    SCSException, AlreadyLoggedIn, ServiceFailure, InvalidService,
+    InvalidProperties {
+    // verificando parametros de entrada
+    if (args.length < 4) {
+      System.out.println(Utils.serverUsage);
+      System.exit(1);
+      return;
+    }
+    // - host
+    String host = args[0];
+    // - porta
+    int port;
+    try {
+      port = Integer.parseInt(args[1]);
+    }
+    catch (NumberFormatException e) {
+      System.out.println(Utils.port);
+      System.exit(1);
+      return;
+    }
+    // - entidade
+    String entity = args[2];
+    // - chave privada
+    String privateKeyFile = args[3];
+    OpenBusPrivateKey privateKey;
+    try {
+      privateKey = OpenBusPrivateKey.createPrivateKeyFromFile(privateKeyFile);
+    }
+    catch (Exception e) {
+      System.out.println(Utils.keypath);
+      e.printStackTrace();
+      System.exit(1);
+      return;
+    }
+
+    // inicializando e configurando o ORB
+    final ORB orb = ORBInitializer.initORB();
+    // - disparando a thread para que o ORB atenda requisições
+    Thread run = new Thread() {
+      @Override
+      public void run() {
+        orb.run();
+      }
+    };
+    run.start();
+    // - criando thread para parar e destruir o ORB ao fim da execução do processo 
+    Thread shutdown = new Thread() {
+      @Override
+      public void run() {
+        orb.shutdown(true);
+        orb.destroy();
+      }
+    };
+    Runtime.getRuntime().addShutdownHook(shutdown);
+
+    // recuperando o gerente de contexto de chamadas à barramentos 
+    OpenBusContext context =
+      (OpenBusContext) orb.resolve_initial_references("OpenBusContext");
+
+    // criando o serviço a ser ofertado
+    // - ativando o POA
+    POA poa = POAHelper.narrow(orb.resolve_initial_references("RootPOA"));
+    poa.the_POAManager().activate();
+    // - construindo os componentes
+    HashMap<Language, ComponentContext> components =
+      new HashMap<GreetingsImpl.Language, ComponentContext>();
+    for (Language language : Language.values()) {
+      ComponentId id =
+        new ComponentId(language.name() + Greetings.class.getSimpleName(),
+          (byte) 1, (byte) 0, (byte) 0, "java");
+      ComponentContext component = new ComponentContext(orb, poa, id);
+      for (Period period : Period.values()) {
+        component.addFacet("Good" + period.name(), GreetingsHelper.id(),
+          new GreetingsImpl(context, language, period));
+      }
+      components.put(language, component);
+    }
+
+    // conectando ao barramento.
+    Connection conn = context.createConnection(host, port);
+    context.setDefaultConnection(conn);
+
+    // autentica-se no barramento
+    boolean failed = false;
+    try {
+      conn.loginByCertificate(entity, privateKey);
+      // registrando serviço no barramento
+      ServiceProperty[] serviceProperties = new ServiceProperty[2];
+      serviceProperties[0] =
+        new ServiceProperty("offer.domain", "Demo Greetings");
+      for (Entry<Language, ComponentContext> entry : components.entrySet()) {
+        serviceProperties[1] =
+          new ServiceProperty("greetings.language", entry.getKey().name());
+        context.getOfferRegistry().registerService(
+          entry.getValue().getIComponent(), serviceProperties);
+      }
+    }
+    // login by certificate
+    catch (AccessDenied e) {
+      failed = true;
+      System.err.println(String.format(
+        "a chave em '%s' não corresponde ao certificado da entidade '%s'",
+        privateKeyFile, entity));
+    }
+    catch (MissingCertificate e) {
+      failed = true;
+      System.err.println(String.format(
+        "a entidade %s não possui um certificado registrado", entity));
+    }
+    // register
+    catch (UnauthorizedFacets e) {
+      failed = true;
+      StringBuffer interfaces = new StringBuffer();
+      for (String facet : e.facets) {
+        interfaces.append("\n  - ");
+        interfaces.append(facet);
+      }
+      System.err
+        .println(String
+          .format(
+            "a entidade '%s' não foi autorizada pelo administrador do barramento a ofertar os serviços: %s",
+            entity, interfaces.toString()));
+    }
+    // bus core
+    catch (ServiceFailure e) {
+      failed = true;
+      System.err.println(String.format(
+        "falha severa no barramento em %s:%s : %s", host, port, e.message));
+    }
+    catch (TRANSIENT e) {
+      failed = true;
+      System.err.println(String.format(
+        "o barramento em %s:%s esta inacessível no momento", host, port));
+    }
+    catch (COMM_FAILURE e) {
+      failed = true;
+      System.err
+        .println("falha de comunicação ao acessar serviços núcleo do barramento");
+    }
+    catch (NO_PERMISSION e) {
+      failed = true;
+      if (e.minor == NoLoginCode.value) {
+        System.err.println(String.format(
+          "não há um login de '%s' válido no momento", entity));
+      }
+    }
+    finally {
+      if (failed) {
+        context.getCurrentConnection().logout();
+        System.exit(1);
+      }
+    }
+
+  }
+}
