16939a8562a8fdff2c367bea0b7efe8a13ff035a
Hugo Roenick <hroenick@tecgraf.puc-rio.br>
2013-10-18 16:56:41 +0000

    [OPENBUS-2386] Propriedades do JacORB sao sobrescritas equivocadamente pela API Java  - removendo a configuracao de propriedades do jacORB que nao sao obrigatorias  - incluindo secao sobre logging do jacorb no documento do assistente
    git-svn-id: https://subversion.tecgraf.puc-rio.br/engdist/openbus/sdk/java/branches/02_00_00@145898 ae0415b3-e90b-0410-900d-d0be9363c56b

--- core/src/tecgraf/openbus/core/ORBBuilder.java
+++ core/src/tecgraf/openbus/core/ORBBuilder.java
@@ -1,70 +1,79 @@
 package tecgraf.openbus.core;
 
-import java.util.Map.Entry;
 import java.util.Properties;
 
 import org.omg.CORBA.ORB;
 
+/**
+ * Classe responsável por inicializar o {@link ORB}
+ * 
+ * @author Tecgraf
+ */
 final class ORBBuilder {
+  /** Argumentos de linha de comando */
   private String[] args;
+  /** Propriedades */
   private Properties props;
 
+  /**
+   * Construtor.
+   */
   public ORBBuilder() {
     this(null, null);
   }
 
+  /**
+   * Construtor
+   * 
+   * @param args argumentos de linha de comando
+   */
   public ORBBuilder(String[] args) {
     this(args, null);
   }
 
+  /**
+   * Construtor
+   * 
+   * @param props propriedades.
+   */
   public ORBBuilder(Properties props) {
     this(null, props);
   }
 
+  /**
+   * Construtor
+   * 
+   * @param args argumentos de linha de comando
+   * @param props propriedades
+   */
   public ORBBuilder(String[] args, Properties props) {
     this.args = args;
-    if (props == null) {
-      this.props = buildDefaultProperties();
-    }
-    else {
-      this.props = buildFromProperties(props);
+    this.props = props;
+    if (this.props == null) {
+      this.props = new Properties();
     }
     this.props.setProperty("org.omg.CORBA.ORBClass", "org.jacorb.orb.ORB");
     this.props.setProperty("org.omg.CORBA.ORBSingletonClass",
       "org.jacorb.orb.ORBSingleton");
   }
 
-  public void setHost(String host) {
-    this.props.put("OAIAddr", host);
-  }
-
-  public void setPort(int port) {
-    this.props.put("OAPort", String.valueOf(port));
-  }
-
+  /**
+   * Configura as informações do inicializador do ORB.
+   * 
+   * @param initializer informações do inicializador.
+   */
   public void addInitializer(ORBInitializerInfo initializer) {
     this.props.put("org.omg.PortableInterceptor.ORBInitializerClass."
       + initializer.getId(), initializer.getClassName());
   }
 
+  /**
+   * Constroi o {@link ORB}. Chama o {@link ORB#init(String[], Properties)}.
+   * 
+   * @return o ORB
+   */
   public ORB build() {
     return ORB.init(this.args, this.props);
   }
 
-  private Properties buildDefaultProperties() {
-    Properties props = new Properties();
-    props.setProperty("jacorb.log.default.verbosity", "1"); // nível de ERROR
-    props
-      .setProperty("jacorb.connection.client.pending_reply_timeout", "30000");
-    return props;
-  }
-
-  private Properties buildFromProperties(Properties props) {
-    Properties result = new Properties(buildDefaultProperties());
-    for (Entry<Object, Object> entry : props.entrySet()) {
-      result.setProperty(String.valueOf(entry.getKey()), String.valueOf(entry
-        .getValue()));
-    }
-    return result;
-  }
 }
